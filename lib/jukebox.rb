817f6505f6012e6dfb16b045fd069cfac5652056
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-20 18:07:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,11 +17,24 @@ def help
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
+
 help
+
 def list (songs)
   songs.each_with_index {|song, index|
     num = index + 1
     puts "#{num}. #{song}"
   }
 end
+
 list(songs)
+
+def play (songs)
+  puts "Please enter a song name or number:"
+  choice = gets.strip
+  songs.each {|song|
+    if song == choice
+      puts song
+    end
+  }
+end
