8f11189a8c6175483cf222ee2a3e031cef439c84
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-20 18:18:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,15 +1,3 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
-
 def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -28,14 +16,19 @@ end
 def play (songs)
   puts "Please enter a song name or number:"
   choice = gets.strip
+  
   songs.each_with_index do |song, index|
     num = index + 1
     if song == choice
-      puts "playing #{song}"
+      valid_song =  "playing #{song}"
     elsif choice.to_i == num
-      puts "playing #{song}"
+      valid_song = "playing #{song}"
     end
   end
+  if valid_song
+    puts valid_song
+  else 
+    puts "Invalid input, please try again"
 end
 
 def exit_jukebox
